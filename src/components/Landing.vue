0eedc4b9c7fde59cefb2ee5e471f6e85e3a3b48c
MichaelSauerman <michael.sauerman@synthesis.co.za>
2024-04-30 15:48:54 +0200

    Added intial code, removed vue bloat

---
+++ src/components/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="landing">
+        <h1>Welcome to the landing page!</h1>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Landing',
+}
+</script>
+
+<style scoped>
+
+</style>
